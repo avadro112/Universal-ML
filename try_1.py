846916df9721b4626f9c0a6165676c2b059648e7
Anant Yadav <66402514+avadro112@users.noreply.github.com>
2021-10-02 19:23:05 +0530

    just_a_try
    i 'll upload code as soon i'll  perfectly implement loops

---
+++ try_1.py
@@ -0,0 +1,41 @@
+#Note ::: Pls Typo ... GITHUB is new to me 
+#import numpy as np
+#import pandas as pd
+
+df = df.read_csv('data.csv')
+
+#optional method to use .loc
+#f = df.ilco[:,m].values
+#f = df.iloc[:,n].values
+
+
+
+#spli#ing the data using scikit Learn 
+
+from sklear.model_selection import train_test_split
+
+#o this for split the deta in y and x
+#se manual method -- with numpy and pandas 
+#use feature scaling 
+import sklearn.preprocessing import StandardScaler
+import sklearn.preprocessing import OneHotEncoder
+
+
+##trian using multi 
+
+#use the conditions for dataset if it contains  multiple >> variables 
+# get the confusion_matrix using sklearn
+# get the accuracy score using slearn
+
+
+
+#using scaled dataset import the the trainer algorithms 
+
+#like ::
+
+from sklearn.linear_model import LinearRegression
+from sklearn.
+
+
+
+
